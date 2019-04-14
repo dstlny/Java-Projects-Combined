093070d88cea880fb6595574523ab94567c72057
Luke Elam <lukeelam1999@icloud.com>
2019-04-14 14:38:40 +0100

    Add files via upload

---
+++ src/PublicInterfaces.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.nio.file.*;
+
+public class PublicInterfaces {
+	
+	public interface HashGenInterface {
+		public void produceFileHash(byte[] fileBytes, int fileSize, int multiple);
+		public void produceDirHash(byte[] fileBytes, int fileSize, int multiple,  long lastModified);
+		public void produceDirMetaHash(int fileSize, int multiple, long lastModified);
+		public long getHash();
+	}
+	
+	interface HashGenExtendedInterface extends HashGenInterface {
+		public long mix(byte a, int c, int b);
+		public long mix(byte a, long c, int b);
+		public long mix(int a, int c, long b);
+	}
+	
+	interface FileAccessInterface {
+		public void singleFile(String inName, String inDirectory, int inChoice2) throws IOException;
+		public void multipleFiles(String inName, String inDirectory, int inChoice2) throws IOException;
+		public void multipleFilesMetaData(String inName, String inDirectory, int inChoice2) throws IOException;
+		public void algorithmSelect(int algorithmChoice);
+		public byte[] readFileContent(String pathname) throws IOException, FileSystemException ;
+	}
+	
+	public interface WriteToDisk {
+		public void WriteFile(String fileToScan)  throws IOException, FileNotFoundException;
+	}
+
+}
